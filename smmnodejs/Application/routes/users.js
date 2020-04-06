901f3a609a0a8004037ce4c1fa4d56cbeec8331e
sunilmathew <s5mathe@homeoffice.wal-mart.com>
2020-04-06 10:32:17 +0530

    Changed user route

--- smmnodejs/Application/routes/users.js
+++ smmnodejs/Application/routes/users.js
@@ -1,10 +1,10 @@
-﻿'use strict';
-var express = require('express');
+﻿"use strict";
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function (req, res) {
-    res.send('respond with a resource');
+router.get("/", function(req, res) {
+  res.send("Sunil connected Git with Azure");
 });
 
 module.exports = router;
